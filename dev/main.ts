6dd9695df8fa572a4aeb3ecce571d6053a5299ee
Clamarque <chabox59@gmail.com>
2016-04-14 15:09:47 +0200

    Fix reload Grand child

--- dev/main.ts
+++ dev/main.ts
@@ -1,7 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {provide} from 'angular2/core';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS,ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [HTTP_PROVIDERS,ROUTER_PROVIDERS,provide(LocationStrategy, {useClass: HashLocationStrategy})]);
 
