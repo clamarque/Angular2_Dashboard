06c47dcba210ff04439a572298064bc7d0f6288d
equilliot <eric.quillliot@numericable.fr>
2016-04-21 11:00:50 +0200

    update

--- dev/login/login.service.spec.ts
+++ dev/login/login.service.spec.ts
@@ -6,3 +6,51 @@ import { Router, RouterLink } from 'angular2/router';
 import 'rxjs/add/operator/map';
 
 
+//Service
+import {LoginService} from "./login.service";
+
+
+
+describe("passwordController", function(){
+    beforeEach(modele("app.component"));
+    
+    var ctrl;
+    
+    beforeEachProviders(inject(function()))
+})
+
+
+
+var modele, params = null;
+
+    beforeEach((){
+        modele = {
+            setParams : function(value){
+                params = value;
+            }
+        };
+    spyOn(modele, 'setParams');
+    modele.setParams('1','test');
+        
+    })
+    
+    it("Call the methode getTeams()", function(){
+        expect(modele.setParams).toHaveBeenCalled();
+    });
+
+
+
+describe('Other test call methode', () =>{
+    let service;
+    
+    beforeEachProviders(() => [LoginService]);
+    
+    beforeEach(inject([LoginService], s => {
+        service = s;
+    }));
+    
+    it('Should return length data from mock', () =>{
+        let data = service.getTeams();
+        expect(data.lenght).toEqual(2);
+    });
+})
