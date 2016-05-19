d049979b0bc4807271e987360d85c46e06938b63
Clamarque <chabox59@gmail.com>
2016-05-19 17:17:37 +0200

    Update router-deprecated to router

--- public/dev/login/login.component.ts
+++ public/dev/login/login.component.ts
@@ -1,44 +1,44 @@
 //Angular
 import {Component} from '@angular/core';
-import {Router, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {Router, ROUTER_DIRECTIVES} from '@angular/router';
 import { CORE_DIRECTIVES, FORM_DIRECTIVES } from '@angular/common';
-
-import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
 //Service
 import {LoginService} from "./login.service";
 
+import {TranslatePipe} from 'ng2-translate/ng2-translate';
+
+
 @Component({
-    selector:'login',
-    providers:[LoginService],
+    selector: 'login',
+    providers: [LoginService],
     templateUrl: './dev/login/login.component.html',
-    directives: [CORE_DIRECTIVES,FORM_DIRECTIVES, ROUTER_DIRECTIVES],
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES],
     pipes: [TranslatePipe]
-    
 })
 
 export class LoginComponent {
-    
+
     msg: string;
     logged: boolean;
-    
-    constructor (public translate: TranslateService, private _loginService : LoginService,private _router: Router ){}
-    
+
+    constructor(private _loginService: LoginService, private _router: Router) { }
+
     login(email, password) {
         let self = this;
-        this._loginService.login(email, password, function(data){
-            if(data){
-                localStorage.setItem('username',email);
+        this._loginService.login(email, password, function (data) {
+            if (data) {
+                localStorage.setItem('username', email);
                 self.msg = "Login Successful";
                 self.logged = true;
-                self._router.parent.navigateByUrl('/Home');
+                self._router.navigate(['/Home']);
                 console.log('Login success');
             }
-            else{
+            else {
                 console.log('error login');
-                self.msg ="Login Failed!";
-                self.logged =false;
+                self.msg = "Login Failed!";
+                self.logged = false;
             }
         })
-    }  
+    }
 }
--- public/dev/login/login.service.ts
+++ public/dev/login/login.service.ts
@@ -1,33 +1,33 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
-import {Router, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+//import {Router, ROUTER_DIRECTIVES} from '@angular/router';
 
 @Injectable()
 export class LoginService {
-    
+
     appUrl: string = "https://blazing-inferno-9370.firebaseio.com/";
 
     firebase = new Firebase("https://blazing-inferno-9370.firebaseio.com/");
-   
-    
-   /* setUser(username: string, password: string){
-        const body = JSON.stringify({username: username, password: password});
-        
-       return this._http.post('https://blazing-inferno-9370.firebaseio.com/user.json', body)
-        .map(response => response.json());
-      */  
-        
-        login(email: string, password: string, callback) {
+
+
+    /* setUser(username: string, password: string){
+         const body = JSON.stringify({username: username, password: password});
+         
+        return this._http.post('https://blazing-inferno-9370.firebaseio.com/user.json', body)
+         .map(response => response.json());
+       */
+
+    login(email: string, password: string, callback) {
         this.firebase.authWithPassword({
             email: email,
             password: password
-        }, function(error, data) {
+        }, function (error, data) {
             if (error) {
                 callback(false)
             } else {
                 return callback(data.token);
             }
-            
+
         })
-    }   
+    }
 }
