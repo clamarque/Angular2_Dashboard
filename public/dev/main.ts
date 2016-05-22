af42232f04c20ddd0005608c6d193f1b1726f9aa
Clamarque <chabox59@gmail.com>
2016-05-22 23:44:20 +0200

    delete import login

--- public/dev/main.ts
+++ public/dev/main.ts
@@ -5,12 +5,11 @@ import {provide, PLATFORM_PIPES} from '@angular/core';
 import {TranslateLoader,TranslateStaticLoader,TRANSLATE_PROVIDERS,TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
 import {BootstrappingComponent} from './bootstrapping/bootstrapping.component';
-import {LoginService} from './login/login.service';
+
 //bootstrap(AuthenticationComponent, [HTTP_PROVIDERS,ROUTER_PROVIDERS,TRANSLATE_PROVIDERS,provide(LocationStrategy, {useClass: HashLocationStrategy}),provide(PLATFORM_PIPES, {useValue: [TranslatePipe], multi:true})]);
 bootstrap(BootstrappingComponent, [
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS,
-    LoginService,
     provide(TranslateLoader, {useFactory: (http:Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'), deps: [Http]}),
     provide(PLATFORM_PIPES, {useValue: [TranslatePipe], multi:true}),
     TRANSLATE_PROVIDERS,
