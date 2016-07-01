baa394f4d529e37c462975699ae82b33d332f0ae
Clamarque <chabox59@gmail.com>
2016-07-02 01:53:49 +0200

    delete ref url firebase

--- public/dev/login/login.service.ts
+++ public/dev/login/login.service.ts
@@ -5,9 +5,7 @@ declare var firebase: any;
 @Injectable()
 
 export class LoginService {
-    
-    appUrl: string = "https://blazing-inferno-9370.firebaseio.com/";
-    
+
     login(email: string, password: string, callback: any) {
         firebase.auth().signInWithEmailAndPassword(email, password).then(
             (user: any) => callback(),
