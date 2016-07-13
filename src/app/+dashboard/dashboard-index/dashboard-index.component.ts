af2120250761360c0a3e47588e832d994b6181d4
Clamarque <chabox59@gmail.com>
2016-07-13 11:51:45 +0200

    adding a dashboard as view default

---
+++ src/app/+dashboard/dashboard-index/dashboard-index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dashboard-index',
+    templateUrl: './app/+dashboard/dashboard-index/dashboard-index.component.html',
+})
+
+export class DashboardIndexComponent {
+    constructor(){ }
+}
